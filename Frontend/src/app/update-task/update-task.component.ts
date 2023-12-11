61914c4eb0d008ca14cc4ccd377cc7bb54f18970
sainaakash <aakash.sain08@gmail.com>
2023-12-11 17:52:37 +0530

    update task component working

--- Frontend/src/app/update-task/update-task.component.ts
+++ Frontend/src/app/update-task/update-task.component.ts
@@ -1,66 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TasksService } from '../tasks.service';
+import { Subscription } from 'rxjs';
+
+// ... (existing imports)
 
 @Component({
   selector: 'app-update-task',
   templateUrl: './update-task.component.html',
   styleUrls: ['./update-task.component.css']
 })
-export class UpdateTaskComponent {
-  angForm : FormGroup;
+export class UpdateTaskComponent implements OnInit, OnDestroy {
+  angForm: FormGroup;
   error: any;
-  subscription: any;
   id: any;
   task: any;
-  tasks: any;
   success: string | undefined;
+  private subscription: Subscription;
 
   constructor(
     private fb: FormBuilder,
-    private route: Router,
+    private router: Router,
     private tasksService: TasksService,
-    private activeRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute
   ) {
-
     this.angForm = this.fb.group({
       id: [''],
       dueDate: [''],
       content: ['', Validators.required],
+      completed: [false], // Assuming completed is a boolean, change the type accordingly
     });
 
-    this.subscription = this.activeRoute.paramMap.subscribe(params => {
+    this.subscription = this.activatedRoute.paramMap.subscribe(params => {
       this.id = params.get('id');
-    })
+    });
   }
 
   ngOnInit(): void {
     this.getSingleTask(this.id);
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   getSingleTask(id: any): void {
     this.tasksService.getTaskById(id)
-      .subscribe((res: any) => {
-        this.task = res;
-        this.angForm.patchValue(this.task);
-        console.log(res);
-      },
-      (err: any) => {
-        this.error = err;
-      });
-  }  
+      .subscribe(
+        (res: any) => {
+          this.task = res;
+          this.angForm.patchValue(this.task);
+          console.log(res);
+        },
+        (err: any) => {
+          this.error = err;
+        }
+      );
+  }
 
-  editData(f: any) {
-    console.log(f.value);
-    this.tasksService.updateTask(this.id, f.value)
-      .subscribe((res: any) => {
-        this.tasks = res;
-        this.success = "Updated successfully";
-        this.route.navigate(['/list-tasks']);
-      },
-      (err: any) => {
-        this.error = err;
-      })
+  editData(): void {
+    if (this.angForm.valid) {
+      this.tasksService.updateTask(this.id, this.angForm.value)
+        .subscribe(
+          (res: any) => {
+            this.success = 'Updated successfully';
+            this.router.navigate(['/list-tasks']);
+          },
+          (err: any) => {
+            this.error = err;
+          }
+        );
+    } else {
+      console.error('Form is invalid');
+    }
   }
 }
